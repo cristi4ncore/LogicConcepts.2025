98ee03db76d654fe6ef49c383d18928942f7955e
Dark <cristi4n2010@gmail.com>
2025-08-21 19:05:15 -0500

    empezando

---
+++ LogicConcepts.2025/IsNumberOddOrNot/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Escriba un número: ");
+ if (int.TryParse(Console.ReadLine(), out int numero))
+    {
+       if (numero % 2 == 0)
+         {
+          Console.WriteLine("El número " + numero + " es PAR.");
+           }
+            else
+            {
+                Console.WriteLine("El número " + numero + " No es PAR.");
+            }
+               }
+               else
+                  {
+                  Console.WriteLine("Eso no es un número válido.");
+                  }
+    
